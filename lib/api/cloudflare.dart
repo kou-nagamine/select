2f5105e942c0fd3e1cd2c0f8772f14eca29c822d
kou-nagamine <136679628+kou-nagamine@users.noreply.github.com>
2024-04-09 09:19:11 +0900

    backend data flow done

---
+++ lib/api/cloudflare.dart
@@ -0,0 +1,31 @@
+import 'package:http/http.dart' as http;
+import "../models/pants.dart";
+import "dart:convert";
+
+class CloudflareConnecter{
+  static const String cloudeflareUrl = 'http://insertinfo.kou6922gool.workers.dev';
+
+  static Future<void> insertData(PantsInfo pantsinfo) async {
+
+    try {
+      final response = await http.post(
+        Uri.parse(cloudeflareUrl),
+        headers: {'Content-Type': 'application/json'},
+        body:jsonEncode({
+          'color': pantsinfo.color,
+          'shape': pantsinfo.shape,
+        })
+      );
+
+      if (response.statusCode == 201) {
+        print('Data inserted successfuly');
+        print('body${response.body}');
+      } else {
+        print('Data insert failed');
+        print('${response.body}');
+      }
+    }catch(e){
+      print('Error: ${e}');
+    }
+  }
+}
