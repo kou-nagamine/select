3179ca8ab103c6d758613fc0f2ab51d521819f0a
kou-nagamine <136679628+kou-nagamine@users.noreply.github.com>
2024-04-09 09:57:47 +0900

    image import done

--- lib/views/resultpage.dart
+++ lib/views/resultpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/pants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Resultpage extends StatelessWidget {
   final PantsInfo pantsinfo;
@@ -8,12 +9,17 @@ class Resultpage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context){
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text("${pantsinfo.shape}"),
-        backgroundColor: pantsinfo.color,
-      )
+      body: Stack(
+        alignment: AlignmentDirectional.centerStart,
+        children: <Widget>[
+          Container(
+            child: SvgPicture.asset(
+              'assets/images/heart.svg',
+            )
+          )
+        ]
+      ),
     );
   }
 }
